0644d5250c237a1ea66ebc32e28afbed5b9937f6
Sergey <123@gmail.com>
2017-02-03 15:03:29 +0400

    Commit 03/02/2017 15:01 Replace Pow(int i) of Pow(int,int j)

--- Abstract.cs
+++ Abstract.cs
@@ -8,7 +8,7 @@ abstract class A
 		abstract public int Add(int i);
 		abstract public int Delete(int i);
 		abstract public int Multiply(int i);
-		abstract public int Pow(int i);
+		abstract public int Pow(int i,int j);
 		abstract public int Sqrt(int i);
 		abstract public int Today();
 	}
