ef4c2b33c241b6ef9c6bd2cfc1744d4940ca76f1
Sergey <123@gmail.com>
2017-02-03 15:08:50 +0400

    Commit 03/02/2017

--- Abstract.cs
+++ Abstract.cs
@@ -10,6 +10,7 @@ abstract class A
 		abstract public int Multiply(int i);
 		abstract public int Pow(int i,int j);
 		abstract public int Sqrt(int i);
-		abstract public int Today();
+		abstract public int TodayFunction();
+		
 	}
 }
